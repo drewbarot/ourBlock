3284b3a3e02b6a0ac0a9fb0be668fcefca92c3b0
Vishal1654 <vishalchopra@macbook-pro.wifi.int.ryerson.ca>
2018-06-09 02:06:26 -0400

    Maps update

--- maps/maps.py
+++ maps/maps.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 import json
-
 import os.path
 import collections
-
 
 data = {}
 
@@ -21,10 +19,9 @@ long = -79.509755
 
 id = 0
 
-if len(list(sorted(data.keys()))) == 0:
-    id = 0
-else:
+if len(list(sorted(data.keys()))) != 0:
     id = int(list(data.keys())[len(list(sorted((data.keys())))) - 1]) + 1
+    
     
 date = datetime.now()
 
@@ -37,7 +34,6 @@ year = date.strftime("%Y")
 hour = date.strftime("%H")
 
 data[id] = {"Year": year, "Month": month, "Day": day, "Hour": hour, "Latitude": lat, "Longitude": lat}
-
 
 ordered_data = collections.OrderedDict(sorted(data.items()))
 
