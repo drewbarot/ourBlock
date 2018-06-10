a89debcb727c75c21c0ce6e4d6f3fb179c28dec5
Lawrence Pang <lawrencepang36@gmail.com>
2018-06-10 11:20:48 -0400

    search and report

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,Response
 from run_prediction import classify
 import requests
+import ast
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-  return render_template('index.html')
 
 @app.route('/post',methods=['POST'])
 def post():
+  form = ast.literal_eval(dict(request.form).keys()[0])
   try:
-    classified,confidence = classify(request.form['Description'])
-  except:
+    classified,confidence = classify(form['Description'])[0]
+  except Exception as e:
+    print(e)
     classified,confidence = '',0
+  data = form
   data['Class'] = classified
   data['Confidence'] = confidence
+  print(data['Class'],data['Confidence'])
   requests.post('https://gony0gqug0.execute-api.us-east-1.amazonaws.com/beta/post',data=data)
+  resp = Response('')
+  resp.headers['Access-Control-Allow-Origin'] = '*'
+  return resp
