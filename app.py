47118052ac01abcea84d205f45fb8eb90719f905
Lawrence Pang <lawrencepang36@gmail.com>
2018-06-10 11:53:30 -0400

    bug fixes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request,Response
 from run_prediction import classify
 import requests
 import ast
+import json
 app = Flask(__name__)
 
 @app.route('/post',methods=['POST'])
@@ -12,11 +13,12 @@ def post():
   except Exception as e:
     print(e)
     classified,confidence = '',0
-  data = form
-  data['Class'] = classified
-  data['Confidence'] = confidence
-  print(data['Class'],data['Confidence'])
-  requests.post('https://gony0gqug0.execute-api.us-east-1.amazonaws.com/beta/post',data=data)
+  data = {}
+  data['body'] = form
+  data['body']['Class'] = classified
+  data['body']['Confidence'] = float(confidence)
+  print(data['body']['Class'],data['body']['Confidence'])
+  requests.post('https://gony0gqug0.execute-api.us-east-1.amazonaws.com/beta/post',json=data)
   resp = Response('')
   resp.headers['Access-Control-Allow-Origin'] = '*'
   return resp
