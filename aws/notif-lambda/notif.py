57b1410e37db41bca13732c98619ddb70cd82045
Lawrence Pang <lawrencepang36@gmail.com>
2018-06-09 21:43:30 -0400

    police view functionality

--- aws/notif-lambda/notif.py
+++ aws/notif-lambda/notif.py
@@ -4,13 +4,23 @@ import json
 def notif(event,context):
   event = json.loads(event['body'])
   sqs = boto3.resource('sqs')
-  queue = sqs.get_queue_by_name(QueueName=event['id'])
   messages = []
-  for message in queue.receive_messages():
-    messages.append(message.body)
-    message.delete()
+  try:
+    queue = sqs.get_queue_by_name(QueueName=event['id'])
+    for message in queue.receive_messages():
+      messages.append(message.body)
+      message.delete()
+  except:
+    print(event['id'])
+  try:
+    all_queue = sqs.get_queue_by_name(QueueName='_all')
+    for message in all_queue.receive_messages():
+      messages.append(message.body)
+      message.delete()
+  except:
+    print('_all')
   return { 
-        'isBase64Encoded': True
+        'isBase64Encoded': True,
         'statusCode': 200,
         'body': json.dumps(messages),
         'headers': {
