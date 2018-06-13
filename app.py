ba38d8f4c59340f6d2abf932cb4844e172e3c783
Charlie Lin <hlin266@uwo.ca>
2018-06-12 23:31:11 -0400

    Added resources, reverted test

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route('/post',methods=['POST'])
 def post():
-  print('HI');
   form = ast.literal_eval(dict(request.form).keys()[0])
   try:
     classified,confidence = classify(form['Description'])[0]
